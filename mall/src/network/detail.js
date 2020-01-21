08f4fed18e6151c7a25ca1fe5dc83262ee4242fb
geallean <geallean@gmail.com>
2020-01-21 17:03:33 +0800

    详情页

--- mall/src/network/detail.js
+++ mall/src/network/detail.js
@@ -8,3 +8,38 @@ export function getDetail (iid) {
     }
   })
 }
+
+// 作用：整合数据 -- 把服务器返回的数据封装到一个类里面
+// 然后new出一个对象来，将对象传到下一个组件里面
+export class Goods {
+  constructor (itemInfo, columns, services) {
+    this.title = itemInfo.title
+    this.desc = itemInfo.desc
+    this.newPrice = itemInfo.price
+    this.oldPrice = itemInfo.oldPrice
+    this.discount = itemInfo.discountDesc
+    this.columns = columns
+    this.services = services
+    this.realPrice = itemInfo.lowNowPrice
+  }
+}
+
+export class Shop {
+  constructor (shopInfo) {
+    this.logo = shopInfo.shopLogo
+    this.name = shopInfo.name
+    this.fans = shopInfo.cFans
+    this.sells = shopInfo.cSells
+    this.score = shopInfo.score
+    this.goodsCount = shopInfo.cGoods
+  }
+}
+
+export class GoodsParam {
+  constructor (info, rule) {
+    // 注: images可能没有值(某些商品有值, 某些没有值)
+    this.image = info.images ? info.images[0] : ''
+    this.infos = info.set // infos是一个数组
+    this.sizes = rule.tables[0] // sizes是一个数组
+  }
+}
--- mall/src/views/detail/Detail.vue
+++ mall/src/views/detail/Detail.vue
@@ -1,15 +1,28 @@
 <template>
-  <div class="Detail">
+  <div id="Detail">
     <!-- 详情{{id}} -->
-    <detail-nav-bar></detail-nav-bar>
-    <detail-swiper :topImages="topImages"></detail-swiper>
+    <detail-nav-bar class="nav"></detail-nav-bar>
+    <!-- content必须设高度 -->
+    <scroll class="scroll" ref="scroll">
+      <detail-swiper :topImages="topImages"></detail-swiper>
+      <detail-base-info :goods="goods"></detail-base-info>
+      <detail-shop-info :shop="shop"></detail-shop-info>
+      <detail-goods-info :detailInfo="detailInfo" @imageLoad="imageLoad"></detail-goods-info>
+      <detail-param-info :paramInfo="paramInfo"></detail-param-info>
+    </scroll>
   </div>
 </template>
 
 <script>
 import DetailNavBar from './childComponents/DetailNavBar'
-import { getDetail } from '@/network/detail'
+import { getDetail, Goods, Shop, GoodsParam } from '@/network/detail'
 import DetailSwiper from './childComponents/DetailSwiper'
+import DetailBaseInfo from './childComponents/DetailBaseInfo'
+import DetailShopInfo from './childComponents/DetailShopInfo'
+import DetailGoodsInfo from './childComponents/DetailGoodsInfo'
+import DetailParamInfo from './childComponents/DetailParamInfo'
+
+import Scroll from '@/components/common/scroll/Scroll'
 
 export default {
   name: 'Detail',
@@ -20,7 +33,11 @@ export default {
     return {
       iid: null,
       res: null,
-      topImages: []
+      topImages: [],
+      goods: {},
+      shop: {},
+      detailInfo: {},
+      paramInfo: {}
     }
   },
   created () {
@@ -29,20 +46,58 @@ export default {
     // 2.根据id请求详情
     getDetail(this.iid).then(res => {
       console.log(res)
-      // 顶部轮播图数据
-      this.topImages = res.data.result.itemInfo.topImages
-      // console.log(this.topImages)
+      const data = res.data.result
+      // 1.获取顶部轮播图数据
+      this.topImages = data.itemInfo.topImages
+
+      // 2.获取商品信息
+      this.goods = new Goods(data.itemInfo, data.columns, data.shopInfo.services)
+      console.log(this.goods)
+
+      // 3.创建店铺信息对象
+      this.shop = new Shop(data.shopInfo)
+      console.log(this.shop)
+
+      // 4.保存商品详情数据
+      this.detailInfo = data.detailInfo
+
+      // 5.获取参数详情
+      this.paramInfo = new GoodsParam(data.itemParams.info, data.itemParams.rule)
+      console.log(this.paramInfo)
+      // console.log(Object.keys(this.paramInfo).length)
     })
   },
   methods: {
-
+    // 当图片加载完后，做刷新,使better-scroll适应图片高度-解决bug
+    imageLoad () {
+      this.$refs.scroll.refresh()
+    }
   },
   components: {
     DetailNavBar,
-    DetailSwiper
+    DetailSwiper,
+    DetailBaseInfo,
+    DetailShopInfo,
+    Scroll,
+    DetailGoodsInfo,
+    DetailParamInfo
   }
 }
 </script>
 
 <style scoped>
+#Detail {
+  position: relative;
+  z-index: 9;
+  background-color: #fff;
+  height: 100vh;
+}
+.nav {
+  position: relative;
+  z-index: 9;
+  background-color: #fff;
+}
+.scroll {
+  height: calc(100% - 44px);
+}
 </style>
